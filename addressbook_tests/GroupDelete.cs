e2a081eda79d0c5fb1fcc7843e2fdcf64849c7c0
SenQuepasa <agentpnz90@gmail.com>
2025-12-19 15:38:03 +0300

    создан базовый класс, в него перенесены общие методы для всех тестов

---
+++ addressbook_tests/GroupDelete.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class GroupDeleteTests : TestBase
+    {
+        [Test]
+        public void GroupDeleteTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            SelectGroup(1);
+            RemoveGroup();
+            ReturnToGroupsPage();
+            Logout();
+        }
+    }
+}
