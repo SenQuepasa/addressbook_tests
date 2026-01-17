3f2ebb7f4793f1f6e82c47a66d1464eee66d4cec
SenQuepasa <agentpnz90@gmail.com>
2026-01-17 11:28:11 +0300

    файл создается пустым

--- addressbook_tests/tests/GroupCreationTests.cs
+++ addressbook_tests/tests/GroupCreationTests.cs
@@ -8,6 +8,7 @@ using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.IO;
 
 namespace WebAddressbookTests
 {
@@ -28,9 +29,27 @@ namespace WebAddressbookTests
             return groups;
         }
 
-        
+        public static IEnumerable<GroupData> GroupDataFromFile()
+        {
+            List<GroupData> groups = new List<GroupData>();
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
+                {
+                    Header = parts[1],
+                    Footer = parts[2]
+                });
+            }
+            return groups;
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        }
+
+
+
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             List<GroupData> oldGroups = app.Groups.GetGroupList();
